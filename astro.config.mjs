b335a5afbdfb131aa9249aa55a789cd4447dde14
PushLimits <joemanuola@gmail.com>
2025-09-29 21:42:15 -0400

    build: add packages to support Astro content support and mdx build: enable strict null check for typescript build: add support for astro check CLI command

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,22 @@
 // @ts-check
+
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
 import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://manuola.com",
+  integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
     resolve: {
       alias: {
         "@components": "/src/components",
         "@icons": "/src/components/icons",
-        "@images": "/src/images",
+        "@assets": "/src/assets",
+        "@data": "/src/data",
         "@layouts": "/src/layouts",
         "@pages": "/src/pages",
         "@styles": "/src/styles"
